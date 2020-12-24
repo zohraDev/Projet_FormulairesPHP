87b4a70488388f142063368a88c8a031e1f82324
zohraDev <zohra.bachi@edu-itescia.fr>
2020-12-24 00:07:03 +0000

    formulaire

--- Formulaire_Inscription.php
+++ Formulaire_Inscription.php
@@ -1,9 +1,38 @@
-Formulaire_Inscription.php
-<?php
-/**
- * Formulaire d'inscription
- * 
- */
+<!DOCTYPE html>
+<html>  
+    <head>
+        <meta  charset="UTF-8" >
+        <title> INSCRITION FORM</title>
+        <link rel ="stylesheet"  href ="Style.css">
+
+    </head>
+    <body>
+        <div class ="FormInscrs">
+            <div class="form-text">Inscription</div>
+            <div class="form-saisie">
+                <form method="post" action="traitement.php">
+                    <label for="name">Enter your name</label>
+                    <input type ="text"  id ="name" name= "name" >
 
 
-?>
+                    <label for="lastname">Enter your laste name</label>
+                    <input type ="text"  id ="lastename" name= "lastname" >
+
+                    <label for="email">Enter your email</label>
+                    <input type ="text"  id ="email" name= "email" >
+
+                    <input type="submit" value="send">
+
+            
+            
+                </form>
+
+            
+            </div>
+        
+        </div>
+    
+    </body>
+
+
+</html>
