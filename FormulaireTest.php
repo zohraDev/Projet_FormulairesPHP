099339b65bdf77a7ae1529170bd6753d59bf377a
zohraDev <zohra.bachi@edu-itescia.fr>
2020-12-25 07:37:57 +0000

    debut formulaireTest
    :q

---
+++ FormulaireTest.php
@@ -0,0 +1,20 @@
+FormulaireTest.php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Formulaire Test</title>
+    </head>
+
+    <body>
+        <form  action="Target.php" method="post" >
+
+
+        
+        </form>
+
+    </body>
+
+
+
+</html>
---
+++ Target.php
@@ -0,0 +1,15 @@
+Target.php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Formulaire Test</title>
+    </head>
+
+    <body>
+
+    </body>
+    
+
+
+</html>
